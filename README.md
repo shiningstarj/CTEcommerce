aab690ff082f586c3583c3a3db4e8b6979d5cb5e
Jordan Williams <jordanwllms178@gmail.com>
2018-04-19 15:01:16 -0400

    More samples, also styling and layout

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Auron
 Simple eCommerce web app
 
-To set up simply run the following:
+To set up simply run the following from the root of the repo:
 ```
 export (set in Windows environment) FLASK_APP=auron
 pip install -e .
--- auron/samples.py
+++ auron/samples.py
@@ -1,4 +1,6 @@
 class Samples():
 	samples = [('GMC Denali Road Bike', 169.99, '21-speed road bike features a lightweight aluminum frame.Step through frame.Soft, sure grip handlebar tape'),
-	('Thorne Research - Multi-Vitamin Elite', 62.00,'COMPREHENSIVE MULTI: Two multi-vitamin/mineral formulas – one for morning, one for evening – that support high performance nutrition')
-	]
+	('Thorne Research - Multi-Vitamin Elite', 62.00,'COMPREHENSIVE MULTI: Two multi-vitamin/mineral formulas – one for morning, one for evening – that support high performance nutrition'),
+	('Benchmark Bouquets Signature Roses and Alstroemeria, With Vase', 37.58, 'Flowers are shipped in bud form for optimal vase life; blooms will open in 2-3 days, as shown in product photo.'),
+	("Godiva Chocolatier Classic Gold Ballotin Chocolate, Great for Mother's Day, 19 Count", 29.00, "Includes Godiva classics like Milk Chocolate Embrace, Dark Mint Medallion and White Chocolate Demitasse"),
+	("Hyperbiotics PRO-15 Probiotics - 60 Daily Time Release Pearls - 15x More Effective than Probiotic Capsules with Patented Delivery Technology - Easy to Swallow Probiotic Supplement", 23.70, "LIFT YOUR MOOD AND ENERGY LEVELS. Fatigue and irritability are often a result of poor vitamin and nutrient absorption. PRO-15 helps create a microenvironment inside your digestive system that promotes full mineral absorption so you can finally feel like your happy, energetic, and vibrant self.")]
